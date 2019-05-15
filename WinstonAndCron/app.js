195fd4ea7613a8c661809bea25ec226f75e0d7e8
Andre <andrewvrosa@gmail.com>
2019-05-15 15:09:13 -0400

    learn Winston and Cron

---
+++ WinstonAndCron/app.js
@@ -0,0 +1,50 @@
+
+//Example from the Express tutorial https://expressjs.com/en/starter/hello-world.html
+// To run the code use->  set DEBUG=myapp:* & npm start
+
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// serve static files in express, 
+// using the absolute path of the directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// basic routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
